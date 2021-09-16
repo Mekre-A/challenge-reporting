23851e2e5491433d7914589b8aaab53f191d651d
Mekre <mekre.abate@gmail.com>
2021-09-16 14:23:25 +0300

    getStudent endpoint now returns a student as JSON based on the route params id field, validator package has been installed to check for incorrect route params

--- api.js
+++ api.js
@@ -1,4 +1,5 @@
 const knex = require('./db')
+const validator = require('validator')
 
 module.exports = {
   getHealth,
@@ -18,7 +19,20 @@ async function getHealth (req, res, next) {
 }
 
 async function getStudent (req, res, next) {
-  throw new Error('This method has not been implemented yet.')
+
+  if(!validator.isInt(req.params.id, {min:1})){
+    return res.status(400).send()
+  }
+
+  knex('students').where('id', req.params.id).asCallback(function(err, student){
+    if(err){
+     console.log(e)
+     return res.status(500).end()
+    }
+    console.log(student)
+   return res.send(student)
+  })
+  
 }
 
 async function getStudentGradesReport (req, res, next) {
