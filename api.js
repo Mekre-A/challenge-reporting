ae9f4e87b118abc749bc68a9cc206a8d7366a3b4
Mekre <mekre.abate@gmail.com>
2021-09-16 15:18:45 +0300

    getStudentGradesReport now returns the student data along with the grades for the available courses

--- api.js
+++ api.js
@@ -1,5 +1,7 @@
 const knex = require('./db')
 const validator = require('validator')
+const fs = require('fs');
+
 
 module.exports = {
   getHealth,
@@ -8,37 +10,76 @@ module.exports = {
   getCourseGradesReport
 }
 
-async function getHealth (req, res, next) {
+async function getHealth(req, res, next) {
   try {
     await knex('students').first()
-    res.json({ success: true })
+    res.json({
+      success: true
+    })
   } catch (e) {
     console.log(e)
     res.status(500).end()
   }
 }
 
-async function getStudent (req, res, next) {
+async function getStudent(req, res, next) {
 
-  if(!validator.isInt(req.params.id, {min:1})){
+  if (!validator.isInt(req.params.id, {
+      min: 1
+    })) {
     return res.status(400).send()
   }
 
-  knex('students').where('id', req.params.id).asCallback(function(err, student){
-    if(err){
-     console.log(e)
-     return res.status(500).end()
+  knex('students').where('id', req.params.id).asCallback(function (error, studentArray) {
+    if (error) {
+      console.log(error)
+      return res.status(500).end()
     }
-    console.log(student)
-   return res.send(student)
+    const student = studentArray[0];
+    return res.send(student)
   })
-  
+
 }
 
-async function getStudentGradesReport (req, res, next) {
-  throw new Error('This method has not been implemented yet.')
+async function getStudentGradesReport(req, res, next) {
+
+  if (!validator.isInt(req.params.id, {
+      min: 1
+    })) {
+    return res.status(400).send()
+  }
+
+
+  knex('students').where('id', req.params.id).asCallback(function (error, studentArray) {
+    if (error) {
+      console.log(error)
+      return res.status(500).end()
+    }
+
+    const student = studentArray[0];
+
+    fs.readFile('grades.json', (error, data) => {
+      if (error) {
+        console.log(error)
+        return res.status(500).end();
+      }
+      let grades = JSON.parse(data);
+
+      grades = grades.filter((json) => json.id === student.id).map((grade) => {
+        grade.id = undefined;
+        return grade;
+      })
+      if(!grades.length){
+        return res.status(404).send();
+      }
+      student.grades = grades
+      return res.send(student);
+    })
+
+
+  })
 }
 
-async function getCourseGradesReport (req, res, next) {
+async function getCourseGradesReport(req, res, next) {
   throw new Error('This method has not been implemented yet.')
 }
