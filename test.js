e0be7aaa44ef5af86c3d1c855ca4b0cd5c4fcb83
Mekre <mekre.abate@gmail.com>
2021-09-16 17:22:45 +0300

    Tests have been written for each endpoint

--- test.js
+++ test.js
@@ -20,6 +20,59 @@ tape('health', async function (t) {
   }
 })
 
+
+tape('Request for student data with valid and existing id should return with 200', (t) => {
+  const url = `${endpoint}/student/22`
+    jsonist.get(url, (error, data, response) =>{
+     t.equal(200, response.statusCode)
+      t.end()
+    })
+})
+
+tape('Request for student data with invalid id should return 400', (t) => {
+  const url = `${endpoint}/student/0`
+    jsonist.get(url, (error, data, response) =>{
+     t.equal(400, response.statusCode)
+      t.end()
+    })
+})
+
+
+tape('Request for grades of a student with valid id should return the grades of a student', (t) => {
+  const url = `${endpoint}/student/1/grades`
+    jsonist.get(url, (error, data, response) =>{
+     t.equal(200, response.statusCode)
+     t.equal(data.grades.length, 4)
+     t.end()
+    })
+})
+
+tape('Request for grades of a student with valid id but no entry in the grades.json should return 404', (t) => {
+  const url = `${endpoint}/student/20/grades`
+    jsonist.get(url, (error, data, response) =>{
+     t.equal(404, response.statusCode)
+      t.end()
+    })
+})
+
+tape('Request for grades of a student with invalid id for grades should return 400', (t) => {
+  const url = `${endpoint}/student/asdf/grades`
+    jsonist.get(url, (error, data, response) =>{
+     t.equal(400, response.statusCode)
+      t.end()
+    })
+})
+
+
+
+tape('Request for all courses with their stats should return 200', (t) => {
+  const url = `${endpoint}/course/all/grades`
+    jsonist.get(url, (error, data, response) =>{
+     t.equal(200, response.statusCode)
+      t.end()
+    })
+})
+
 tape('cleanup', function (t) {
   server.closeDB()
   server.close()
